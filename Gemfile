504f9aa2d6974721c9af3da02ca7f89d97be1999
Maciej Lorens <mckl@poczta.fm>
2013-08-04 18:01:40 +0200

    init

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0.rc1'
+
+gem 'sass-rails', '~> 4.0.0.rc1'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'hirb'
+gem 'therubyracer', platforms: :ruby
+gem 'mysql2'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.0.1'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+gem 'unicorn'
+gem 'capistrano', group: :development
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webdevleague::Application.config.secret_key_base = 'd4f982cff94b62489be4c3cbaf522f89f0c3473f523d809aa07e2ae625fd181ac70c8622abbdf5d1187ddb4789c2de177efb99bccd54630ee091d44253bad6f9'
